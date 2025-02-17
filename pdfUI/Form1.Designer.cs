e9a095d0adec41ed5633948d6b58aa9cd94ddcdb
sumeyyevarmis <84784675+sumeyyevarmis@users.noreply.github.com>
2025-02-17 17:16:27 +0300

    md file read

--- pdfUI/Form1.Designer.cs
+++ pdfUI/Form1.Designer.cs
@@ -43,6 +43,12 @@
             pdfViewer1=new PdfiumViewer.PdfViewer();
             tabPage2=new TabPage();
             tableLayoutPanel1=new TableLayoutPanel();
+            tableLayoutPanel4=new TableLayoutPanel();
+            panel3=new Panel();
+            button4=new Button();
+            richTextBox2=new RichTextBox();
+            panel4=new Panel();
+            richTextBox3=new RichTextBox();
             panel1.SuspendLayout();
             tabControl1.SuspendLayout();
             tabPage1.SuspendLayout();
@@ -50,6 +56,10 @@
             tableLayoutPanel3.SuspendLayout();
             panel2.SuspendLayout();
             tabPage2.SuspendLayout();
+            tableLayoutPanel1.SuspendLayout();
+            tableLayoutPanel4.SuspendLayout();
+            panel3.SuspendLayout();
+            panel4.SuspendLayout();
             SuspendLayout();
             // 
             // panel1
@@ -197,6 +207,7 @@
             pdfViewer1.Location=new Point(571, 3);
             pdfViewer1.Margin=new Padding(4, 3, 4, 3);
             pdfViewer1.Name="pdfViewer1";
+            pdfViewer1.ShowToolbar=false;
             pdfViewer1.Size=new Size(559, 561);
             pdfViewer1.TabIndex=1;
             // 
@@ -216,6 +227,8 @@
             tableLayoutPanel1.ColumnCount=2;
             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
             tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel1.Controls.Add(tableLayoutPanel4, 0, 0);
+            tableLayoutPanel1.Controls.Add(panel4, 1, 0);
             tableLayoutPanel1.Dock=DockStyle.Fill;
             tableLayoutPanel1.Location=new Point(3, 3);
             tableLayoutPanel1.Name="tableLayoutPanel1";
@@ -223,6 +236,72 @@
             tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
             tableLayoutPanel1.Size=new Size(1134, 567);
             tableLayoutPanel1.TabIndex=0;
+            // 
+            // tableLayoutPanel4
+            // 
+            tableLayoutPanel4.ColumnCount=1;
+            tableLayoutPanel4.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 50F));
+            tableLayoutPanel4.Controls.Add(panel3, 0, 0);
+            tableLayoutPanel4.Controls.Add(richTextBox2, 0, 1);
+            tableLayoutPanel4.Dock=DockStyle.Fill;
+            tableLayoutPanel4.Location=new Point(3, 3);
+            tableLayoutPanel4.Name="tableLayoutPanel4";
+            tableLayoutPanel4.RowCount=2;
+            tableLayoutPanel4.RowStyles.Add(new RowStyle(SizeType.Percent, 14.6167555F));
+            tableLayoutPanel4.RowStyles.Add(new RowStyle(SizeType.Percent, 85.38325F));
+            tableLayoutPanel4.Size=new Size(561, 561);
+            tableLayoutPanel4.TabIndex=0;
+            // 
+            // panel3
+            // 
+            panel3.Controls.Add(button4);
+            panel3.Dock=DockStyle.Fill;
+            panel3.Location=new Point(3, 3);
+            panel3.Name="panel3";
+            panel3.Size=new Size(555, 76);
+            panel3.TabIndex=0;
+            // 
+            // button4
+            // 
+            button4.Location=new Point(3, 3);
+            button4.Name="button4";
+            button4.Size=new Size(75, 23);
+            button4.TabIndex=1;
+            button4.Text="Open md";
+            button4.UseVisualStyleBackColor=true;
+            button4.Click+=button4_Click;
+            // 
+            // richTextBox2
+            // 
+            richTextBox2.BackColor=Color.Black;
+            richTextBox2.Dock=DockStyle.Fill;
+            richTextBox2.ForeColor=Color.White;
+            richTextBox2.Location=new Point(3, 85);
+            richTextBox2.Name="richTextBox2";
+            richTextBox2.Size=new Size(555, 473);
+            richTextBox2.TabIndex=1;
+            richTextBox2.Text="";
+            // 
+            // panel4
+            // 
+            panel4.BackColor=SystemColors.WindowFrame;
+            panel4.Controls.Add(richTextBox3);
+            panel4.Dock=DockStyle.Fill;
+            panel4.Location=new Point(570, 3);
+            panel4.Name="panel4";
+            panel4.Size=new Size(561, 561);
+            panel4.TabIndex=1;
+            // 
+            // richTextBox3
+            // 
+            richTextBox3.BackColor=Color.Black;
+            richTextBox3.Dock=DockStyle.Fill;
+            richTextBox3.ForeColor=Color.White;
+            richTextBox3.Location=new Point(0, 0);
+            richTextBox3.Name="richTextBox3";
+            richTextBox3.Size=new Size(561, 561);
+            richTextBox3.TabIndex=2;
+            richTextBox3.Text="";
             // 
             // Form1
             // 
@@ -241,6 +320,10 @@
             panel2.ResumeLayout(false);
             panel2.PerformLayout();
             tabPage2.ResumeLayout(false);
+            tableLayoutPanel1.ResumeLayout(false);
+            tableLayoutPanel4.ResumeLayout(false);
+            panel3.ResumeLayout(false);
+            panel4.ResumeLayout(false);
             ResumeLayout(false);
         }
 
@@ -261,5 +344,11 @@
         private TextBox textBox1;
         private Label label1;
         private Button button3;
+        private TableLayoutPanel tableLayoutPanel4;
+        private Panel panel3;
+        private RichTextBox richTextBox2;
+        private Button button4;
+        private Panel panel4;
+        private RichTextBox richTextBox3;
     }
 }
--- pdfUI/Form1.cs
+++ pdfUI/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using PdfiumViewer;
+using System.Text.RegularExpressions;
 
 namespace pdfUI
 {
@@ -60,6 +61,48 @@ namespace pdfUI
                 // Seçilen dosyanın tam yolunu TextBox'a yaz
                 textBox1.Text = openFileDialog.FileName;
             }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Markdown Files (*.md)|*.md|All Files (*.*)|*.*";
+            openFileDialog.Title = "MD Dosyası Seç";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    string filePath = openFileDialog.FileName;
+                    string fileContent = File.ReadAllText(filePath);
+                    richTextBox2.Text = fileContent;
+
+                    string markdownContent = File.ReadAllText(filePath);
+
+                    // Markdown'ı RichTextBox'a RTF formatında ekleyelim
+                    richTextBox3.Rtf = ConvertMarkdownToRtf(markdownContent);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("file is not read: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+        private string ConvertMarkdownToRtf(string markdown)
+        {
+            // RTF Başlangıcı
+            string rtfHeader = @"{\rtf1\ansi\deff0{\colortbl;\red0\green0\blue0;\red0\green0\blue255;} ";
+
+            // **Kalın** -> \b Kalın \b0
+            markdown = Regex.Replace(markdown, @"\*\*(.*?)\*\*", @"\b $1 \b0");
+
+            // *İtalik* -> \i İtalik \i0
+            markdown = Regex.Replace(markdown, @"\*(.*?)\*", @"\i $1 \i0");
+
+            // # Başlık -> Büyük ve Mavi Renk
+            markdown = Regex.Replace(markdown, @"^# (.*?)$", @"\cf2\b $1 \b0", RegexOptions.Multiline);
+
+            return rtfHeader + markdown + "}";
         }
     }
 }
